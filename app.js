912ab1d2a404251967e3d405785ba9aba9e38374
Pizzan8t0r <https://github.com/Pizzan8t0r>
2023-05-10 19:01:12 -0700

    more stuff now the file paths

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+// ile path to where Node.js is installed on your computer
+const commandLineArgs = process.argv;
+// file path for the file that was executed
+console.log(commandLineArgs);
+//[
+   // '/usr/local/bin/node',
+   // '/Users/billymandy/Desktop/portfolio-generator/app.js'
+ // ]
+
 console.log("testing testing, hello node!");
 const message = 'Hellooo nurseee';
 const sum = 5 + 3;

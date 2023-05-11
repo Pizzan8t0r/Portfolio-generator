f754bd350ce2d26058832e68086717c39d93778c
Pizzan8t0r <https://github.com/Pizzan8t0r>
2023-05-10 18:57:13 -0700

    playing around with the command line to spit funnystuff and a sum

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("testing testing, hello node!");
+const message = 'Hellooo nurseee';
+const sum = 5 + 3;
+console.log(message);
+console.log(sum);
+// created variables and logged them as output
+//-> testing testing, hello node!
+//-> Hellooo nurseee
+//-> 8

fb48519d97d754ab69671b814fc93a2070983de5
Pizzan8t0r <https://github.com/Pizzan8t0r>
2023-05-11 19:37:53 -0700

    interpolated the command-line arguments into the HTML using  and

--- app.js
+++ app.js
@@ -11,8 +11,20 @@ const [name, github] = profileDataArgs;
 // create multi-line input for the previous example
 const generatePage = (userName, githubName) => {
     return `
-    Name: ${userName}
-    Github: ${githubName}
+    <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> html Portfolio thing </title>
+  </head>
+
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
     
     `;
 };

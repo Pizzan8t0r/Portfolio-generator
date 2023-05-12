3b0fc198147c02bf7b6cafc4dc5568b33e6b96f0
Pizzan8t0r <https://github.com/Pizzan8t0r>
2023-05-11 19:33:56 -0700

    one lining as well as some destructuring to our code

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const profileDataArgs = process.argv.slice(2);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+//const name = profileDataArgs[0];
+//const github = profileDataArgs[1];
+// destructuring assignment assigns elements of an array to variable names in a single expression
+// one-lining the previous statements above using destructuring
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+// To make this function dynamic, we could add arguments to the function expression, then insert the data into the string using interpolation, 
+// which is the substitution of text for a variable we build into the string.
+//const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+// create multi-line input for the previous example
+const generatePage = (userName, githubName) => {
+    return `
+    Name: ${userName}
+    Github: ${githubName}
+    
+    `;
 };
 
-printProfileData(profileDataArgs);
+console.log(name, github);
+console.log(generatePage(name, github));

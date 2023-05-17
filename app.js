14e5dafde3c1977808d5407f4f476c090e12980a
Pizzan8t0r <https://github.com/Pizzan8t0r>
2023-05-17 15:48:54 -0700

    need to fix some errors in the scopes

--- app.js
+++ app.js
@@ -24,8 +24,56 @@ const inquirer = require('inquirer');
 
     //console.log('Portfolio complete. Check out the index.html to see the output');
 //});
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username (Required)',
+        validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log('Please enter your GitHub username!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+      },
+      {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => confirmAbout
+      }
+    ]);
+  };
+
 const promptProject = portfolioData => {
-portfolioData.projects = [];
+// If there's no 'projects' array property, create one
+// if control statement to guarantee the array is initialized only on the first pass
+// data collection system
+if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
 console.log(`
 =================
 Add a New Project
@@ -34,44 +82,79 @@ Add a New Project
 return inquirer.prompt([
 // inquirer's prompt method can receive an array of objects in its argument, known as the question object. 
 //the function returns a running of inquire.prompt(), thus returning what it returns, which is a Promise.
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the name of your project?'
-          },
-          {
-            type: 'input',
-            name: 'description',
-            message: 'Provide a description of the project (Required)'
-          },
-          {
-            type: 'checkbox',
-            name: 'languages',
-            message: 'What did you build this project with? (Check all that apply)',
-            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-          },
-          {
-            type: 'input',
-            name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
-          },
-          {
-            type: 'confirm',
-            name: 'feature',
-            message: 'Would you like to feature this project?',
-            default: false
-          },
-          {
-            type: 'confirm',
-            name: 'confirmAddProject',
-            message: 'Would you like to enter another project?',
-            default: false
-          }
-  ]);
+{
+    type: 'input',
+    name: 'name',
+    message: 'What is the name of your project? (Required)',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('You need to enter a project name!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of the project (Required)',
+    validate: descriptionInput => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log('You need to enter a project description!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'checkbox',
+    name: 'languages',
+    message: 'What did you this project with? (Check all that apply)',
+    choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+  },
+  {
+    type: 'input',
+    name: 'link',
+    message: 'Enter the GitHub link to your project. (Required)',
+    validate: linkInput => {
+      if (linkInput) {
+        return true;
+      } else {
+        console.log('You need to enter a project GitHub link!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'confirm',
+    name: 'feature',
+    message: 'Would you like to feature this project?',
+    default: false
+  },
+  {
+    type: 'confirm',
+    name: 'confirmAddProject',
+    message: 'Would you like to enter another project?',
+    default: false
+  }
+])
+.then(projectData => {
+  portfolioData.projects.push(projectData);
+  if (projectData.confirmAddProject) {
+    return promptProject(portfolioData);
+  } else {
+    return portfolioData;
+  }
+ });
+  
 };
 //the promise will resolve with a .then method
 promptUser()
 // Using Promises, we can chain the functions together using the then() method
 .then(answers => console.log(answers))
 .then(promptProject)
-.then(projectAnswers => console.log(projectAnswers));
+.then(projectAnswers =>  {
+    console.log(projectAnswers)); 
+}
